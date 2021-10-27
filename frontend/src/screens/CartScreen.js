bafa7007183b9be47609c57ef6f3050ca108b9a9
Jeremy <tommyhilfiger12@hotmail.com>
2021-10-27 00:03:47 -0500

    original

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,17 +1,28 @@
 import './CartScreen.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // Components
 import CartItem from '../components/CartItem';
 
 const CartScreen = () => {
+	const dispatch = useDispatch();
+
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+
 	return (
 		<div className="cartscreen">
 			<div className="cartscreen__left">
 				<h2>Shopping Cart</h2>
-				<CartItem />
-				<CartItem />
-				<CartItem />
-				<CartItem />
+				{cartItems.length === 0 ? (
+					<div>
+						Your cart is empty <Link to="/">Go Back</Link>
+					</div>
+				) : (
+					cartItems.map((item) => <CartItem item={/>)
+				)}
 			</div>
 			<div className="cartscreen__right">
 				<div className="cartscreen__info">
